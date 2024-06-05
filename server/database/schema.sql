612b78bf443a3e7c69c5188ef75ff05c06f56143
Loic legoff <loic95280@gmail.com>
2024-06-05 11:31:16 +0200

    add dev

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -17,5 +17,6 @@ create table recipe (
   step varchar(100) not null,
   step_time varchar(100) not null,
   cooking_time varchar(100) not null,
+  menu_id int unsigned not null,
   foreign key(menu_id) references menu(id)
 );
