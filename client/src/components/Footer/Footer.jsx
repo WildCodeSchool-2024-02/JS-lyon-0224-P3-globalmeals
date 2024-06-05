3ef550c696b4884b1c7e7ab9fe0bcee759d76dcf
Loic legoff <loic95280@gmail.com>
2024-06-06 00:07:58 +0200

    create footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <section className={styles.footer}>
+        <div>
+          <p>Copyright© - 2024</p>
+        </div>
+      </section>
+    </footer>
+  );
+}
+export default Footer;
