aa7d8ee77ad30a360067c59c76bb132d796a8585
djibmsa@gmail.com <djibmsa@gmail.com>
2024-06-12 16:52:48 +0200

    fix prettier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/Footer/Footer";
+import Connexion from "./components/formulaire/connexion";
 
 function App() {
   return (
     <>
       <Header />
       <h1>NavBar</h1>
+      <Connexion />
       <main className="container">
         <Outlet />
         <Footer />
---
+++ client/src/pages/connexionPage.jsx
@@ -0,0 +1,11 @@
+import Connexion from "../components/formulaire/connexion";
+
+function ConnexionPage() {
+  return (
+    <div>
+      <Connexion />
+    </div>
+  );
+}
+
+export default ConnexionPage;
