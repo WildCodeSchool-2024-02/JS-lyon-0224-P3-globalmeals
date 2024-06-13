777defb7a288d2f816e3cfa8246b6ace3936f082
Alp Hamurcu <alp_hamurcu@yahoo.com>
2024-06-13 16:56:29 +0200

    route accueil

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 import NavbarToggle from "./components/navbar/NavbarToggle";
 import Footer from "./components/Footer/Footer";
-import Continents from "./components/continents/Continents";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Header />
       <main className="container">
         <NavbarToggle />
-        <Continents />
         <Outlet />
       </main>
       <Footer />
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Menu from "./pages/menu";
+import Continents from "./components/accueil/Accueil";
 
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
 
     children: [
+      {
+        path: "/",
+        element: <Continents />,
+      },
       {
         loader: async () => fetch(`http://localhost:3310/api/menu`),
         path: "/menu",
