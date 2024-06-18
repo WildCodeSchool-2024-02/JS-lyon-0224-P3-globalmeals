8994575e4f9a3c7ac5a022c73a188b0873968f5c
djibmsa@gmail.com <djibmsa@gmail.com>
2024-06-18 15:02:58 +0200

    update fixed

--- client/src/components/formulaire/connexion.jsx
+++ client/src/components/formulaire/connexion.jsx
@@ -2,30 +2,33 @@ import { useState } from "react";
 import "./connexion.css";
 
 function Connexion() {
-  const [connexion, setConnexion] = useState(false)
+  const [connexion, setConnexion] = useState(false);
 
   const handlClick = () => {
-    setConnexion(!connexion)
-  }
+    setConnexion(!connexion);
+  };
   return (
     <div>
       <form className="form">
         <h1>Connexion</h1>
 
-        <label className="registre" htmlFor="pseudo"><p>Pseudo</p>
+        <label className="registrer" htmlFor="pseudo">
+          <p>Pseudo</p>
           <input id="pseudo" placeholder="Pseudo" type="text" />
         </label>
 
-        <label className="adresse" htmlFor="mail"><p>Adresse mail</p>
+        <label className="adresse" htmlFor="mail">
+          <p>Adresse mail</p>
           <input id="mail" placeholder="Adresse mail" type="text" />
         </label>
 
-        <label className="mot" htmlFor="passe"><p>Mot de passe</p>
+        <label className="mot" htmlFor="passe">
+          <p>Mot de passe</p>
           <input id="passe" placeholder="Mot de passe" type="text" />
         </label>
 
-        <button type="button"  className="validate" onClick={handlClick}>
-         Suivant
+        <button type="button" className="validate" onClick={handlClick}>
+          Suivant
         </button>
       </form>
     </div>
