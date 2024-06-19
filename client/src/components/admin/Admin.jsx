f92b7bb20ba2b3d625f4add05ee497fa6b0005f7
Alp Hamurcu <alp_hamurcu@yahoo.com>
2024-06-19 10:05:51 +0200

    reset form after validation

--- client/src/components/admin/Admin.jsx
+++ client/src/components/admin/Admin.jsx
@@ -13,11 +13,18 @@ function Admin() {
     e.target.style.height = `${e.target.scrollHeight}px`;
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Le formulaire a été validé avec succès !");
+    e.target.reset();
+    setSelectedContinent("");
+  };
+
   return (
     <div className="createMenu">
-      <div>
-        <h1>Création d'un menu</h1>
-        <form className="adminContinent">
+      <h1>Création d'un menu</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="adminContinent">
           <label htmlFor="continent">
             Continents:
             <select
@@ -42,96 +49,96 @@ function Admin() {
               onInput={adjustTextareaHeight}
             />
           </label>
-        </form>
-      </div>
-      <div>
-        <h2>Entrée</h2>
-        <form className="newStarter">
-          <label htmlFor="starterName">
-            Nom de l'entrée:
-            <textarea
-              id="starterName"
-              placeholder="Nom de l'entrée"
-              onInput={adjustTextareaHeight}
-            />
-          </label>
-          <label htmlFor="starterIngredients">
-            Ingrédients:
-            <textarea
-              id="starterIngredients"
-              placeholder="Ingrédients"
-              onInput={adjustTextareaHeight}
-            />
-          </label>
-          <label htmlFor="starterSteps">
-            Étapes:
-            <textarea
-              id="starterSteps"
-              placeholder="Étapes"
-              onInput={adjustTextareaHeight}
-            />
-          </label>
-        </form>
-      </div>
-      <div>
-        <h2>Plat</h2>
-        <form className="newDish">
-          <label htmlFor="dishName">
-            Nom du plat:
-            <textarea
-              id="dishName"
-              placeholder="Nom du Plat"
-              onInput={adjustTextareaHeight}
-            />
-          </label>
-          <label htmlFor="dishIngredients">
-            Ingrédients:
-            <textarea
-              id="dishIngredients"
-              placeholder="Ingrédients"
-              onInput={adjustTextareaHeight}
-            />
-          </label>
-          <label htmlFor="dishSteps">
-            Étapes:
-            <textarea
-              id="dishSteps"
-              placeholder="Étapes"
-              onInput={adjustTextareaHeight}
-            />
-          </label>
-        </form>
-      </div>
-      <div>
-        <h2>Dessert</h2>
-        <form className="newDessert">
-          <label htmlFor="dessertName">
-            Nom du dessert:
-            <textarea
-              id="dessertName"
-              placeholder="Nom du Dessert"
-              onInput={adjustTextareaHeight}
-            />
-          </label>
-          <label htmlFor="dessertIngredients">
-            Ingrédients:
-            <textarea
-              id="dessertIngredients"
-              placeholder="Ingrédients"
-              onInput={adjustTextareaHeight}
-            />
-          </label>
-          <label htmlFor="dessertSteps">
-            Étapes:
-            <textarea
-              id="dessertSteps"
-              placeholder="Étapes"
-              onInput={adjustTextareaHeight}
-            />
-          </label>
-        </form>
-      </div>
-      <button type="button">Valider</button>
+        </div>
+        <div>
+          <h2>Entrée</h2>
+          <div className="newStarter">
+            <label htmlFor="starterName">
+              Nom de l'entrée:
+              <textarea
+                id="starterName"
+                placeholder="Nom de l'entrée"
+                onInput={adjustTextareaHeight}
+              />
+            </label>
+            <label htmlFor="starterIngredients">
+              Ingrédients:
+              <textarea
+                id="starterIngredients"
+                placeholder="Ingrédients"
+                onInput={adjustTextareaHeight}
+              />
+            </label>
+            <label htmlFor="starterSteps">
+              Étapes:
+              <textarea
+                id="starterSteps"
+                placeholder="Étapes"
+                onInput={adjustTextareaHeight}
+              />
+            </label>
+          </div>
+        </div>
+        <div>
+          <h2>Plat</h2>
+          <div className="newDish">
+            <label htmlFor="dishName">
+              Nom du plat:
+              <textarea
+                id="dishName"
+                placeholder="Nom du Plat"
+                onInput={adjustTextareaHeight}
+              />
+            </label>
+            <label htmlFor="dishIngredients">
+              Ingrédients:
+              <textarea
+                id="dishIngredients"
+                placeholder="Ingrédients"
+                onInput={adjustTextareaHeight}
+              />
+            </label>
+            <label htmlFor="dishSteps">
+              Étapes:
+              <textarea
+                id="dishSteps"
+                placeholder="Étapes"
+                onInput={adjustTextareaHeight}
+              />
+            </label>
+          </div>
+        </div>
+        <div>
+          <h2>Dessert</h2>
+          <div className="newDessert">
+            <label htmlFor="dessertName">
+              Nom du dessert:
+              <textarea
+                id="dessertName"
+                placeholder="Nom du Dessert"
+                onInput={adjustTextareaHeight}
+              />
+            </label>
+            <label htmlFor="dessertIngredients">
+              Ingrédients:
+              <textarea
+                id="dessertIngredients"
+                placeholder="Ingrédients"
+                onInput={adjustTextareaHeight}
+              />
+            </label>
+            <label htmlFor="dessertSteps">
+              Étapes:
+              <textarea
+                id="dessertSteps"
+                placeholder="Étapes"
+                onInput={adjustTextareaHeight}
+              />
+            </label>
+          </div>
+        </div>
+        <button type="submit">Valider</button>
+      </form>
     </div>
   );
 }
